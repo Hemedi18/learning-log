baa8c4cde2a4e11927f4cc1dab073322457bb4e2
Your GitHub Username <brightfutureskills08@gmail.com>
2026-01-22 17:17:05 +0300

    solving error of login

--- users/urls.py
+++ users/urls.py
@@ -7,6 +7,7 @@ app_name = 'users'
 urlpatterns = [
     # Registration page.
     path('register/', views.register, name='register'),
+    path('logout/', views.logout_view, name='logout'),
 
     # Include default auth urls.
     path('', include('django.contrib.auth.urls')),
